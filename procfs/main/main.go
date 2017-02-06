eba73605e6d36e29ad74eb99b132df7c445acd0c
Carlos Villela <cvillela@digitalocean.com>
2017-02-06 17:48:52 -0500

    Error handling, prettier output

--- procfs/main/main.go
+++ procfs/main/main.go
@@ -10,32 +10,41 @@ import (
 func main() {
 	processes := map[int]uint64{}
 	previousTotalTime := totalCPUTime()
-	allProcs, _ := procfs.NewProcProc()
+	allProcs, err := procfs.NewProcProc()
+	if err != nil {
+		panic(err)
+	}
 	for _, p := range allProcs {
 		processes[p.PID] = uint64(p.UTime + p.STime)
 	}
 
-	fmt.Println("PID,Command,Utilization")
+	fmt.Printf("%-5s %-10s %6s %6s\n", "PID", "Command", "CPU %", "Time")
 	for {
-		time.Sleep(1 * time.Second)
-
 		totalTime := totalCPUTime()
-		fmt.Println(totalTime)
-		allProcs, _ := procfs.NewProcProc()
+		allProcs, err := procfs.NewProcProc()
+		if err != nil {
+			panic(err)
+		}
+
 		for _, p := range allProcs {
 			t := uint64(p.UTime + p.STime)
 			utilization := 100 * (t - processes[p.PID]) / (totalTime - previousTotalTime)
-			fmt.Printf("%5d %15s %5d%% %d\n", p.PID, p.Comm, utilization, t)
+			fmt.Printf("%5d %-10s %5d%% %6d\n", p.PID, p.Comm, utilization, t)
 			processes[p.PID] = t
 		}
 		previousTotalTime = totalTime
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func totalCPUTime() uint64 {
 	var cpu procfs.CPU
 
-	s, _ := procfs.NewStat()
+	s, err := procfs.NewStat()
+	if err != nil {
+		panic(err)
+	}
+
 	for _, c := range s.CPUS {
 		if c.CPU == "cpu" {
 			cpu = c
