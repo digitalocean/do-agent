117ef7aad4bc987debf7fc42f5c9d5ac8be2066f
Brett <blockloop@users.noreply.github.com>
2018-12-14 16:43:02 -0600

    rm old init scripts after_install (#69)

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # vim: noexpandtab
 
 main() {
 	if command -v apt-get 2&>/dev/null; then
-		apt-get update -qq
-		apt-get install -q -y --only-upgrade do-agent
+		apt-get -qq update
+		apt-get -qq install -y --only-upgrade do-agent
 	elif command -v yum 2&>/dev/null; then
 		yum -q -y update do-agent
 	fi
