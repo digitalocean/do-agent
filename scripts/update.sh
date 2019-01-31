2c0506c53a8e4f4007289ea5c92e3a913c791cc6
Brett <blockloop@users.noreply.github.com>
2019-01-31 12:34:06 -0600

    only update do-agent repository in update.sh (#82)

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,7 @@
 
 main() {
 	if command -v apt-get 2&>/dev/null; then
-		apt-get -qq update
+		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 		apt-get -qq install -y --only-upgrade do-agent
 	elif command -v yum 2&>/dev/null; then
 		yum -q -y update do-agent
