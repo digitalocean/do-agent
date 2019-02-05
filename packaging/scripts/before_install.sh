6dba876fd17cc3c79c06f567e5f0c3b597162d10
Brett <blockloop@users.noreply.github.com>
2019-02-05 12:15:38 -0600

    create user in before_install.sh (#86)

--- packaging/scripts/before_install.sh
+++ packaging/scripts/before_install.sh
@@ -9,14 +9,27 @@
 set -ue
 
 SVC_NAME=do-agent
+USERNAME=do-agent
 
-main () {
+main() {
+	create_user
+
 	if command -v systemctl >/dev/null 2>&1; then
-		systemctl stop ${SVC_NAME} || true
+		echo "Stopping systemctl service..."
+		systemctl stop ${SVC_NAME} 2>/dev/null || true
 	elif command -v initctl >/dev/null 2>&1; then
-		initctl stop ${SVC_NAME} || true
+		echo "Stopping initctl service..."
+		initctl stop ${SVC_NAME} 2>/dev/null || true
 	else
-		echo "Unknown init system" > /dev/stderr
+		echo "ERROR: Unknown init system" > /dev/stderr
+	fi
+}
+
+create_user() {
+	# create the user if it doesn't already exist
+	if ! getent passwd $USERNAME >/dev/null 2>&1; then
+		echo "Creating $USERNAME user..."
+		adduser --system $USERNAME
 	fi
 }
 
