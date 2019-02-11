56437e088826fd9a4b29e3b02874c9ff9a8123de
Thor <thansen@digitalocean.com>
2019-02-11 10:14:13 -0600

    updated whitelist

--- cmd/do-agent/config.go
+++ cmd/do-agent/config.go
@@ -45,14 +45,19 @@ var (
 )
 
 var k8sWhitelist = map[string]bool{
-	"kube_pod_container_resource_requests_cpu_cores":    true,
-	"kube_pod_container_resource_requests_memory_bytes": true,
-	"kube_node_status_allocatable_cpu_cores":            true,
-	"kube_node_status_allocatable_memory_bytes":         true,
-	"kube_pod_container_status_waiting":                 true,
-	"kube_pod_container_status_running":                 true,
-	"kube_pod_container_status_ready":                   true,
-	"kube_pod_container_status_restarts_total":          true,
+	"kube_deployment_spec_replicas":               true,
+	"kube_deployment_status_replicas_available":   true,
+	"kube_deployment_status_replicas_unavailable": true,
+
+	"kube_daemonset_status_desired_number_scheduled": true,
+	"kube_daemonset_status_number_available":         true,
+	"kube_daemonset_status_number_unavailable":       true,
+
+	"kube_statefulset_replicas":              true,
+	"kube_statefulset_status_replicas_ready": true,
+
+	"kube_node_status_allocatable": true,
+	"kube_node_status_capacity":    true,
 }
 
 const (
