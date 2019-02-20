2af667a34d9f5e9bbeb6ccdef8a0d0e9a5e199a7
Thor <thansen@digitalocean.com>
2019-02-20 15:54:39 -0600

    updated whitelist

--- cmd/do-agent/whitelist.go
+++ cmd/do-agent/whitelist.go
@@ -16,4 +16,16 @@ var k8sWhitelist = map[string]bool{
 	"kube_node_status_capacity":    true,
 }
 
-var postgresWhitelist = map[string]bool{}
+var postgresWhitelist = map[string]bool{
+
+	"postgresql_pg_stat_database_blks_hit":                 true,
+	"postgresql_pg_stat_database_blks_read":                true,
+	"net_tcp_activeopens":                                  true,
+	"postgresql_pg_stat_activity_conn_count":               true,
+	"postgresql_pg_stat_activity_oldest_conn_age":          true,
+	"postgresql_pg_stat_activity_oldest_running_query_age": true,
+	"net_packets_recv":                                     true,
+	"net_packets_sent":                                     true,
+	"postgresql_pg_stat_database_deadlocks":                true,
+	"postgresql_pg_stat_replication_flush_diff":            true,
+}
