7307be5f362679af848bd44b28156db02fa7ee62
Thor <thansen@digitalocean.com>
2019-02-21 12:49:29 -0600

    updated postgres whitelist

--- cmd/do-agent/whitelist.go
+++ cmd/do-agent/whitelist.go
@@ -18,14 +18,16 @@ var k8sWhitelist = map[string]bool{
 
 var postgresWhitelist = map[string]bool{
 
-	"postgresql_pg_stat_database_blks_hit":                 true,
-	"postgresql_pg_stat_database_blks_read":                true,
-	"net_tcp_activeopens":                                  true,
-	"postgresql_pg_stat_activity_conn_count":               true,
-	"postgresql_pg_stat_activity_oldest_conn_age":          true,
-	"postgresql_pg_stat_activity_oldest_running_query_age": true,
-	"net_packets_recv":                                     true,
-	"net_packets_sent":                                     true,
-	"postgresql_pg_stat_database_deadlocks":                true,
-	"postgresql_pg_stat_replication_flush_diff":            true,
+	"postgresql_pg_stat_activity_conn_count":       true,
+	"postgresql_pg_stat_database_blks_hit":         true,
+	"postgresql_pg_stat_database_blks_read":        true,
+	"postgresql_pg_stat_database_deadlocks":        true,
+	"postgresql_pg_stat_replication_bytes_diff":    true,
+	"postgresql_pg_stat_user_tables_idx_scan":      true,
+	"postgresql_pg_stat_user_tables_n_tup_ins":     true,
+	"postgresql_pg_stat_user_tables_n_tup_upd":     true,
+	"postgresql_pg_stat_user_tables_seq_scan":      true,
+	"postgresql_pg_stat_user_tables_n_tup_del":     true,
+	"postgresql_pg_stat_user_tables_idx_tup_fetch": true,
+	"postgresql_pg_stat_user_tables_seq_tup_read":  true,
 }
