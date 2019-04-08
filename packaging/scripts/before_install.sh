6712f22aaa5ead2069b57eb29e7c688b651e90c6
Brett Jones <blockloop@users.noreply.github.com>
2019-04-08 08:51:00 -0500

    before_install: adduser -> useradd for compatability

--- packaging/scripts/before_install.sh
+++ packaging/scripts/before_install.sh
@@ -30,7 +30,7 @@ create_user() {
 	# create the user if it doesn't already exist
 	if ! getent passwd $USERNAME >/dev/null 2>&1; then
 		echo "Creating $USERNAME user..."
-		adduser -M --system $USERNAME
+		useradd -M --system $USERNAME
 	fi
 }
 
