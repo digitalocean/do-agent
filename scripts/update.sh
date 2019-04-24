108135f9949f7f1384e6c0fbc31e659aff997d6b
Brett Jones <blockloop@users.noreply.github.com>
2019-04-24 10:41:31 -0500

    fixes #131

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,7 @@
 
 main() {
 	if command -v apt-get 2&>/dev/null; then
-		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list"
+		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="1"
 		apt-get -qq install -y --only-upgrade do-agent
 	elif command -v yum 2&>/dev/null; then
 		yum --disablerepo="*" --enablerepo="digitalocean-agent" makecache
