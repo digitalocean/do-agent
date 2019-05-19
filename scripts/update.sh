64ad0518c8eff9366be71a320f8c071c0d6c27f2
Aitor Alonso <aalonso@aalonso.eu>
2019-05-19 18:38:36 +0200

    Fixes #150

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,7 @@
 # vim: noexpandtab
 
 main() {
+	export DEBIAN_FRONTEND="noninteractive"
 	if command -v apt-get 2&>/dev/null; then
 		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="1"
 		apt-get -qq install -y --only-upgrade do-agent
