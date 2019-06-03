8589974f8efc8e18c6acebea0b646edc1d7267aa
Brett Snyder <bsnyder@digitalocean.com>
2019-06-03 09:03:15 -0500

    Export debian non-interactive front-end first (#162)
    * Export debian non-interactive front-end first
    
    * remove cron jitter

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,8 +2,6 @@
 # vim: noexpandtab
 
 main() {
-	rnd=$(( RANDOM % 900 ))
-	sleep $rnd
 	export DEBIAN_FRONTEND="noninteractive"
 	if command -v apt-get 2&>/dev/null; then
 		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="1"
