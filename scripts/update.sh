0fbc16aba64fd157a6c880fa4e2a591f773a1c67
Brett Snyder <bsnyder@digitalocean.com>
2019-06-03 08:51:30 -0500

    Export debian non-interactive front-end first

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,9 +2,9 @@
 # vim: noexpandtab
 
 main() {
+	export DEBIAN_FRONTEND="noninteractive"
 	rnd=$(( RANDOM % 900 ))
 	sleep $rnd
-	export DEBIAN_FRONTEND="noninteractive"
 	if command -v apt-get 2&>/dev/null; then
 		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="1"
 		apt-get -qq install -y --only-upgrade do-agent
