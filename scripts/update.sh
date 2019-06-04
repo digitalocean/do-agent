90286216a070d19711095c9391e38fa86eaccb8c
Brett <blockloop@users.noreply.github.com>
2019-06-04 10:50:42 -0500

    add package update jitter for nightly cron (#163)

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,9 @@
 # vim: noexpandtab
 
 main() {
+	# add some jitter to prevent overloading the packaging machines
+	sleep $(( RANDOM % 900 ))
+
 	export DEBIAN_FRONTEND="noninteractive"
 	if command -v apt-get 2&>/dev/null; then
 		apt-get -qq update -o Dir::Etc::sourcelist="sources.list.d/digitalocean-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="1"
